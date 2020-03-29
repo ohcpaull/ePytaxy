19ae6776a845e6e8c7d6c8617d6c883dc7b41c5b
Oliver Paull <o.paull@student.unsw.edu.au>
2020-03-30 10:18:43 +1100

    fixing issues

--- epytaxy/__init__.py
+++
@@ -1,5 +0,0 @@
-from .epytaxy import Substrate, Film
-
-from .mat_data import *
-
-from .utils import structure_factor, atomic_scattering_factor
--- epytaxy/setup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
-
-setup(
-    name="epytaxy", version="0.1", author="Oliver Paull", packages=["epytaxy"],
-)
-
